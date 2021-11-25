ed8f13cf9e730f4f58388375508cba94233497d9
Alisson Rafael Barbosa <alissonrafaelbarbosa@gmail.com>
2021-11-24 22:28:03 -0300

    mudanças bot zangado

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -3,39 +3,36 @@ from Bots.Bot import Bot
 class BotZangado(Bot):
     def __init__(self,nome):
         self.__nome = nome
+        self.__comandos = {"1":"Olá, tudo bem ? ", "2":"Como você está ?", "3":"Quero um conselho", "4": "Adeus"}
 
-
-    @property 
+    @property
     def nome(self):
         return self.__nome
 
     @nome.setter
     def nome(self, nome):
-        self.nome = nome
+        self.__nome = nome
+
+    @property
+    def comandos(self):
+        return self.__comandos
 
     def apresentacao(self):
-        return 'Meu nome é',self.__nome,'e e estou com muita RAIVA!!'
-    def mostra_comandos(self):
-        print('1: Ola, tudo bem?')
-        print('2: Come você está?')
-        print('3: Me de um conselho')
-        print('4: Adeus')
-    
+        return f'GRRRRRR Meu nome é {self.__nome} e não me incomode!'
+
     def executa_comando(self,cmd):
-        
+
         if cmd == 1:
             print('E o que tem de bom?!')
         elif cmd == 2:
-            print('Com muita raiva!')
+            print('Não interesa!')
         elif cmd == 3:
             print('Não tenho filho deste tamanho')
         elif cmd == 4:
             print('Até nunca mais!')
 
     def boas_vindas(self):
-        return 'Não acredito que você me escolheu!'
+        return f' {self.nome} diz: Não acredito que você me escolheu!'
 
     def despedida(self):
-        return 'Finalmente, até nunca mais '
-
-
+        return f' {self.nome} diz: Finalmente, até nunca mais! '

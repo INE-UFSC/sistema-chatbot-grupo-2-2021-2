ec15cb37b59a170b6a11f79dd1d8fe139b2236d6
Paulo Eller <pauloebehr@gmail.com>
2021-11-29 19:07:08 -0300

    Alteração do botfeliz

--- Bots/BotFeliz.py
+++ Bots/BotFeliz.py
@@ -1,41 +1,26 @@
 from Bots.Bot import Bot
 
-
 class BotFeliz(Bot):
     def __init__(self, nome):
-        self.__nome = nome
-        self.__comandos = {"1": "Bom dia",
-                           "2": "Qual o sue nome?",
-                           "3": "Quero um conselho",
-                           "4": "Adeus"}
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
+        super().__init__(nome)
 
     def apresentacao(self):
-        return f"Oii, meu nome é {self.__nome}, vamos conversar?"
+        return f"Oii, meu nome é {self.nome}, vamos conversar?"
 
     def executa_comando(self, cmd):
-        if cmd == 1:
-            print("Bomm diaaa flor do dia s2 s2 s2, o sol está lindo hoje")
-        elif cmd == 2:
-            print(f"Meu nome é {self.__nome}, e o seu? Vamos ser amigos!?!?!?")
-        elif cmd == 3:
-            print("Você está pensando demais, apenas aproveite a vida e seja feliz")
-        elif cmd == 4:
-            self.despedida
+        if cmd == "1":
+            return f'{self.nome} diz: Bomm diaaa flor do dia s2 s2 s2, o sol está lindo hoje'
+        elif cmd == "2":
+            return f'{self.nome} diz: Meu nome é {self.nome}, e o seu? Vamos ser amigos!?!?!?'
+        elif cmd == "3":
+            return f' {self.nome} diz: Você está pensando demais, apenas aproveite a vida e seja feliz'
+        elif cmd == "4":
+            return self.despedida()
         else:
-            print('Comando não encontrado')
+            return 'Comando não encontrado'
 
     def boas_vindas(self):
-        boas_vindas = f"{self.__nome} diz: Ebaa, que legal que me escolhesse"
-        print(boas_vindas)
+        return f' {self.nome} diz: Ebaa, que legal que me escolhesse'
 
     def despedida(self):
-        despedida = "Ahh, que pena, foi legal nossa conversa, até a próxima!"
-        print(despedida)
+        return f' {self.nome} diz: Ahh, que pena, foi legal nossa conversa, até a próxima!'

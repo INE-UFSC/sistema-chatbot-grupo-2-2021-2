e3e0edcf3b5dde16f02f3f46f633d5ea413683a7
Alisson Rafael Barbosa <alissonrafaelbarbosa@gmail.com>
2021-11-29 19:32:20 -0300

    Bot zangado corrigido

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -3,34 +3,26 @@ from Bots.Bot import Bot
 
 class BotZangado(Bot):
     def __init__(self, nome):
-        self.__nome = nome
+        super().__init__(nome)
         self.__comandos = {"1": "Olá, tudo bem ? ",
                            "2": "Como você está ?", "3": "Quero um conselho", "4": "Adeus"}
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
 
     @property
     def comandos(self):
         return self.__comandos
 
     def apresentacao(self):
-        return f'GRRRRRR Meu nome é {self.__nome} e não me incomode!'
+        return f'GRRRRRR Meu nome é {self.nome} e não me incomode!'
 
     def executa_comando(self, cmd):
-        if cmd == 1:
+        if cmd == '1':
             print('E o que tem de bom?!')
-        elif cmd == 2:
+        elif cmd == '2':
             print('Não interesa!')
-        elif cmd == 3:
+        elif cmd == '3':
             print('Não tenho filho deste tamanho')
-        elif cmd == 4:
-            self.despedida
+        elif cmd == '4':
+            self.despedida()
         else:
             print('Comando não encontrado')
 

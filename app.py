ed2f4cae0550ca4f6017fd0213ec73fd0ccb2dd6
Larissa Justen <larissajusten@gmail.com>
2021-11-30 11:33:19 -0300

    Add BotFeliz do grupo4

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotFeliz import BotFeliz
 from Bots.BotCansado import BotCansado
+from Bots.BotLegal import BotLegal
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotZangado('Luke'), BotFeliz('João'), BotFeliz('Gabriel'), BotCansado('Pedro (Grupo 3)')]
+lista_bots = [BotZangado("Yoda"), BotZangado('Luke'), BotFeliz('João'), BotFeliz('Gabriel'), BotCansado('Pedro (Grupo 3)'), BotLegal('Feijao (grupo 4)')]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()

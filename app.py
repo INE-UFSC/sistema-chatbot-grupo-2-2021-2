3f576e588c7ff69f74255f768db2ef8e0bb9b61b
Rafael Vargas <rafael.s.vargas18@gmail.com>
2021-11-30 17:32:13 -0400

    Corrindo erro de sintaxe, faltava um )

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ from Bots.BotMinerin import BotMinerin
 from Bots.BotCansado import BotCansado
 from Bots.BotLegal import BotLegal
 
-###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotZangado('Luke'), BotFeliz('João'), BotFeliz('Gabriel'), BotManezinho('Augusto'), BotMinerin('Thiago'),BotLegal('Feijao (grupo 4)',BotCansado('Pedro (Grupo 3)')]
+# construa a lista de bots disponíveis aqui
+lista_bots = [BotZangado("Yoda"), BotZangado('Luke'), BotFeliz('João'), BotFeliz('Gabriel'),
+              BotManezinho('Augusto'), BotMinerin(
+                  'Thiago'), BotLegal('Feijao (grupo 4)'),
+              BotCansado('Pedro (Grupo 3)')]
 
 
-sys = scb.SistemaChatBot("CrazyBots",lista_bots)
+sys = scb.SistemaChatBot("CrazyBots", lista_bots)
 sys.inicio()
